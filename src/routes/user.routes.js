544141732596f67944af02681c491d4bd6e3eb95
Satyam19113 <Satyamkr1354@gmail.com>
2024-05-16 23:41:11 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const router = express.Router() 
-const {registerUser, logoutUser, loginUser, refreshAccessToken} = require("../controllers/user.controller.js") 
+const {registerUser, 
+    logoutUser, 
+    loginUser, 
+    refreshAccessToken, 
+    getCurrentUser, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    changeCurrentPasword, 
+    updateAccountDetails,
+     updateUserAvatar} = require("../controllers/user.controller.js") 
 
 const upload = require("../middlewares/multer.middleware.js")
 const verifyJWT = require("../middlewares/auth.middleware.js")
@@ -30,5 +40,19 @@ router.route("/logout").post(verifyJWT, logoutUser)
 //write an article on what is difference bw access token and refresh token
 // on hashnode platform
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPasword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage )
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 module.exports = router 
